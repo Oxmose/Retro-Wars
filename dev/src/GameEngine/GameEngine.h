ce4c4462c2bb8e772c161aa3595a2183ef13b93c
Tristan Sterin <tristan.sterin@gmail.com>
2015-11-16 13:42:48 +0100

    Swap

--- dev/src/GameEngine/GameEngine.h
+++ dev/src/GameEngine/GameEngine.h
@@ -1,6 +1,6 @@
 /**
  *
- * @file Settings.h
+ * @file GameEngine.h
  *
  * @authors : T. Stérin, A. Torres
  *
@@ -8,73 +8,67 @@
  *
  * @version : 1.0
  *
- * @brief Settings class header file
+ * @brief Game engine header file
  *
- * @see Settings.cpp
+ * @see GameEngine.cpp
  *
  **/
 
-#ifndef DEF_SETTINGS_H
-#define DEF_SETTINGS_H
+#ifndef DEF_GAMEENGINE_H
+#define DEF_GAMEENGINE_H
 
+#include <SFML/Graphics.hpp>	// sf::RenderWindow, sf::VideoMode
 #include <string>				// std::string
+
+#include "GameEngine.h"			// nsGameEngine::GameEngine
+#include "../GraphicEngine/GraphicEngine.h"		// nsGraphicEngine::GraphicEngine
 
 /// @namespace nsGameEngine
 /// @brief The GameEngine namespace.
 /// @details The GameEngine namespace gather all the classes that are related to the game engine.
 namespace nsGameEngine
 {
-
-    /// @def CONFIG_FILE_PATH
-    /// @brief The (relative) path of the config file at execution.
-    #define CONFIG_FILE_PATH "config.cfg"
-    
-	/// @class Settings Settings.h
-	/// @brief The settings class of the game
-	/// @details The Setting class will allow to load and save settings for the game.
-    /// It uses a simple text file to load and save user's settings.
-	class Settings
+	/// @class GameEngine GameEngine.h
+	/// @brief The main class of the game
+	/// @details This is the game engine. This class manages the whole lifecycle of the game.
+    /// It manages the links between the diferent modules such as the graphic engine, the audio angine, etc...
+    /// The class also process all the computing part of the game.
+	class GameEngine
 	{
 		public:
-            /// @fn Settings(const unsigned int & p_width, const unsigned int & p_height, const std::string & p_title) noexcept;
-            /// @brief The default constructor of the Settings class.
-            /// @details The constructor create a Settings object that will allow to easily manage in-game settings.
-			Settings() noexcept;
+            /// @fn GameEngine(const unsigned int & p_width, const unsigned int & p_height, const std::string & p_title) noexcept;
+            /// @param p_width The width of the window.
+            /// @param p_height The height of the window.
+            /// @param p_title The title of the window.
+            /// @brief The detailed constructor of the GameEngine class.
+            /// @details It creates a new instance of the game, load some basics parameters and initialize the window context.
+            /// It will also initialize the graphic engine.
+			GameEngine(const unsigned int & p_width, const unsigned int & p_height, const std::string & p_title) noexcept;
 
-            /// @fn ~Settings();
+            /// @fn ~GameEngine();
             /// @brief The destructor.
-            /// @details It will free the memory.
-			~Settings();
+            /// @details It will get rid of the window context pointer and the other module's pointers.
+			~GameEngine();
 
-            /// @fn void load() noexcept;
-            /// @brief Loads the settings from a file.
-            /// @details The function will load the configuration file in the memory if it is not already done.
-            /// Once loaded, the settings can be acceded by using the differents getters of the class.
-			void load() noexcept;
+            /// @fn void frame();
+            /// @brief The main loop.
+            /// @details The frame function is the main loop of the game, where all the game management will take place.
+			void frame() noexcept;
 
-
-            /*
-             *  SETTERS AND GETTERS
-             */
-
-            unsigned int getWindowWidth();
-            unsigned int getWindowHeight();
-            std::string getWindowTitle();
-            
-            void setWindowWidth(const unsigned int &p_width);
-            void setWindowHeight(const unsigned int &p_height);
-            void setWindowTitle(const std::string &p_windowTitle);
 			
 		private:
-            /// @brief The width of the window.
-            unsigned int m_windowWidth;
-            
-            /// @brief The height of the window.
-            unsigned int m_windowHeight;
+            /// @brief The window dimension variable.
+			sf::VideoMode 	m_windowDim;
 
-            /// @brief The title of the window.
-            std::string m_windowTitle;
+            /// @brief The window title.
+			std::string 	m_windowTitle;
+            
+            /// @brief The pointer to the window object.
+			sf::RenderWindow* m_window;
+        
+            /// @brief The pointer to the graphic engine.
+			nsGraphicEngine::GraphicEngine*	m_graphicEngine;
 	};
 }
 
-#endif /* DEF_SETTINGS_H */
+#endif /* DEF_GAMEENGINE_H */
--- dev/src/GameEngine/Settings.h
+++ dev/src/GameEngine/Settings.h
@@ -1,6 +1,6 @@
 /**
  *
- * @file GameEngine.h
+ * @file Settings.h
  *
  * @authors : T. Stérin, A. Torres
  *
@@ -8,67 +8,73 @@
  *
  * @version : 1.0
  *
- * @brief Game engine header file
+ * @brief Settings class header file
  *
- * @see GameEngine.cpp
+ * @see Settings.cpp
  *
  **/
 
-#ifndef DEF_GAMEENGINE_H
-#define DEF_GAMEENGINE_H
+#ifndef DEF_SETTINGS_H
+#define DEF_SETTINGS_H
 
-#include <SFML/Graphics.hpp>	// sf::RenderWindow, sf::VideoMode
 #include <string>				// std::string
-
-#include "GameEngine.h"			// nsGameEngine::GameEngine
-#include "../GraphicEngine/GraphicEngine.h"		// nsGraphicEngine::GraphicEngine
 
 /// @namespace nsGameEngine
 /// @brief The GameEngine namespace.
 /// @details The GameEngine namespace gather all the classes that are related to the game engine.
 namespace nsGameEngine
 {
-	/// @class GameEngine GameEngine.h
-	/// @brief The main class of the game
-	/// @details This is the game engine. This class manages the whole lifecycle of the game.
-    /// It manages the links between the diferent modules such as the graphic engine, the audio angine, etc...
-    /// The class also process all the computing part of the game.
-	class GameEngine
+
+    /// @def CONFIG_FILE_PATH
+    /// @brief The (relative) path of the config file at execution.
+    #define CONFIG_FILE_PATH "config.cfg"
+    
+	/// @class Settings Settings.h
+	/// @brief The settings class of the game
+	/// @details The Setting class will allow to load and save settings for the game.
+    /// It uses a simple text file to load and save user's settings.
+	class Settings
 	{
 		public:
-            /// @fn GameEngine(const unsigned int & p_width, const unsigned int & p_height, const std::string & p_title) noexcept;
-            /// @param p_width The width of the window.
-            /// @param p_height The height of the window.
-            /// @param p_title The title of the window.
-            /// @brief The detailed constructor of the GameEngine class.
-            /// @details It creates a new instance of the game, load some basics parameters and initialize the window context.
-            /// It will also initialize the graphic engine.
-			GameEngine(const unsigned int & p_width, const unsigned int & p_height, const std::string & p_title) noexcept;
+            /// @fn Settings(const unsigned int & p_width, const unsigned int & p_height, const std::string & p_title) noexcept;
+            /// @brief The default constructor of the Settings class.
+            /// @details The constructor create a Settings object that will allow to easily manage in-game settings.
+			Settings() noexcept;
 
-            /// @fn ~GameEngine();
+            /// @fn ~Settings();
             /// @brief The destructor.
-            /// @details It will get rid of the window context pointer and the other module's pointers.
-			~GameEngine();
+            /// @details It will free the memory.
+			~Settings();
 
-            /// @fn void frame();
-            /// @brief The main loop.
-            /// @details The frame function is the main loop of the game, where all the game management will take place.
-			void frame() noexcept;
+            /// @fn void load() noexcept;
+            /// @brief Loads the settings from a file.
+            /// @details The function will load the configuration file in the memory if it is not already done.
+            /// Once loaded, the settings can be acceded by using the differents getters of the class.
+			void load() noexcept;
 
+
+            /*
+             *  SETTERS AND GETTERS
+             */
+
+            unsigned int getWindowWidth();
+            unsigned int getWindowHeight();
+            std::string getWindowTitle();
+            
+            void setWindowWidth(const unsigned int &p_width);
+            void setWindowHeight(const unsigned int &p_height);
+            void setWindowTitle(const std::string &p_windowTitle);
 			
 		private:
-            /// @brief The window dimension variable.
-			sf::VideoMode 	m_windowDim;
-
-            /// @brief The window title.
-			std::string 	m_windowTitle;
+            /// @brief The width of the window.
+            unsigned int m_windowWidth;
             
-            /// @brief The pointer to the window object.
-			sf::RenderWindow* m_window;
-        
-            /// @brief The pointer to the graphic engine.
-			nsGraphicEngine::GraphicEngine*	m_graphicEngine;
+            /// @brief The height of the window.
+            unsigned int m_windowHeight;
+
+            /// @brief The title of the window.
+            std::string m_windowTitle;
 	};
 }
 
-#endif /* DEF_GAMEENGINE_H */
+#endif /* DEF_SETTINGS_H */
