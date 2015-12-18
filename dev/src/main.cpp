0e5c1674facd6b416a058858c67b8af9e0043d44
Oxmose <alexy.torresa@gmail.com>
2015-12-18 20:49:13 +0100

    uni

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -15,21 +15,23 @@ int main(int argc, char** argv)
 {
     try
     {  
-	  
+	string address = "127.0.0.1"; 
         MapEngine mapEngine("first-map.tmx");
         cout << "Loaded map : " << mapEngine.getPlayers().size() << " players." <<endl;
-        NetEngine netEngine("140.77.183.88", 5003);
+        NetEngine netEngine("127.0.0.1", 5000);
 	PLAYER_TYPE type = RED;
+	netEngine.setIsServer(true);
 	if (argc > 1)
-	{           
-		netEngine.setIsServer(true);
-		type = BLUE;
+	{
+		address = "192.168.2.7"; 
+		PLAYER_TYPE type = BLUE;
+		netEngine.setIsServer(false);
 	}
 	
         if (netEngine.launch("okok", type, &mapEngine))
         {
             cout << "Loaded server on 127.0.0.1:5000.\nLoaded client : " << "Alexy" << " is " << type << endl;
-            GameEngine gameEngine(30*16, 20*16 + 75, "Retro Wars", &mapEngine, RED, &netEngine);
+            GameEngine gameEngine(30*16, 20*16 + 75, "Retro Wars", &mapEngine, type, &netEngine);
             netEngine.setNotifier(&gameEngine);
             cout << "Loaded game engine" << endl;
             gameEngine.frame();
