abf1abae95f909be8c27a51794b8eee01b8ecff1
Tristan Sterin <tristan.sterin@gmail.com>
2015-12-30 20:58:31 +0100

    ft

--- dev/bin/OSXRelease/launch.sh
+++ dev/bin/OSXRelease/launch.sh
@@ -1,2 +1,2 @@
-export DYLD_LIBRARY_PATH=lib/tinyxml2-master:lib/tmxparser-master:lib/SFML-2.3.2-osx-clang-universal/lib
+export DYLD_LIBRARY_PATH=lib/tinyxml2-master:lib/tmxparser-master:lib/SFML-2.3.2-osx-clang-universal/lib:lib/
 ./Retrowars 5000
