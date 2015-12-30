4431a9f2da2d8c88388c74ac0467e9fb590c4e09
Tristan Sterin <tristan.sterin@gmail.com>
2015-12-30 21:31:19 +0100

    audio

--- dev/bin/OSXRelease/launch.sh
+++ dev/bin/OSXRelease/launch.sh
@@ -1,2 +1,3 @@
+install_name_tool -change @rpath/../Frameworks/freetype.framework/Versions/A/freetype lib/libfreetype.dylib lib/SFML-2.3.2-osx-clang-universal/lib/libsfml-graphics.2.3.2.dylib
 export DYLD_LIBRARY_PATH=lib/tinyxml2-master:lib/tmxparser-master:lib/SFML-2.3.2-osx-clang-universal/lib:lib/
 ./Retrowars 5000
