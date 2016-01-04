35cf08691c83ab79033e787b87ae56c5cf0a9439
Oxmose <alexy.torresa@gmail.com>
2016-01-04 15:15:34 +0100

    Fix

--- dev/bin/LinuxRelease/prelauch.sh
+++ dev/bin/LinuxRelease/prelauch.sh
@@ -1,1 +1,1 @@
-export LD_LIBRARY_PATH=lib/tinyxml2-master:lib/tmxparser-master:lib/SFML_LINUX/lib
+export LD_LIBRARY_PATH=lib/tinyxml2-master:lib/tmxparser-master:lib/SFML_LINUX/lib:$LD_LIBRARY_PATH
