9b322a75c5c9580e85ea896048c4c9c17c6559c6
Alexy Torres <alexy@alexytdeb>
2016-01-07 21:58:49 +0100

    Clean left : GameEngine(25% left) and World (100% left)

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -68,6 +68,7 @@ int main(int argc, char** argv)
         else if(argc == 3)
         {
             
+        // In this case not server
             isServer = false;
             playerType = BLUE;
 
