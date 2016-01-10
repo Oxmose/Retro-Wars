fb8ec31602ae8d50d4fc4bf1f44f6a598c38298e
Tristan Sterin <tristan.sterin@gmail.com>
2016-01-10 20:13:59 +0100

    fdi

---
+++ dev/BUILD/bin/OSXRelease/RetroWars.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/GameEngine.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/GameEngine.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/Player.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/Player.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/Terrain.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/Terrain.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/Unit.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/Unit.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/World.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/World.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/GraphicEngine/GraphicEngine.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GraphicEngine/GraphicEngine.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/MD5Engine/md5.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/MD5Engine/md5.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/MD5Engine/md5wrapper.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/MD5Engine/md5wrapper.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/MapEngine/MapEngine.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/MapEngine/MapEngine.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/NetEngine/NetEngine.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/NetEngine/NetEngine.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/NetEngine/Server.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/NetEngine/Server.o"
+  "/Users/tristansterin/Documents/Retro-Wars/dev/src/main.cpp" "/Users/tristansterin/Documents/Retro-Wars/dev/BUILD/bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "../lib/OSX/tinyxml2-master"
+  "../lib/OSX/tmxparser-master"
+  "../lib/OSX/SFML-2.3.2-osx-clang-universal/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ dev/BUILD/bin/OSXRelease/RetroWars.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/GameEngine.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/Player.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/Terrain.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/Unit.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GameEngine/World.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/GraphicEngine/GraphicEngine.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/main.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/MapEngine/MapEngine.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/MD5Engine/md5.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/MD5Engine/md5wrapper.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/NetEngine/NetEngine.o"
+  "CMakeFiles/../bin/OSXRelease/RetroWars.dir/Users/tristansterin/Documents/Retro-Wars/dev/src/NetEngine/Server.o"
+  "../bin/OSXRelease/RetroWars.pdb"
+  "../bin/OSXRelease/RetroWars"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/../bin/OSXRelease/RetroWars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
